3185e03935285f0290e50e9267a88f2b7ec15645
Scott Duffy <duffyscottc@gmail.com>
2016-12-23 22:42:05 -0500

    First lines of code

--- src/sduffy/MainRunner.java
+++ src/sduffy/MainRunner.java
@@ -1,7 +1,10 @@
 package sduffy;
 
+import java.util.Scanner;
+
 public class MainRunner {
 	public static void main (String [] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		int [] orig = new int[3]; //Something to hold the origin coordinates
 	}
 }
